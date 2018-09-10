effcf8a89a20e8164f128a135503d616b5642920
kzhuo123 <league@WTS14-2.attlocal.net>
2018-09-10 16:54:41 -0700

    level 1

---
+++ src/Houses.java
@@ -0,0 +1,59 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+public static void main(String[] args) {
+ Robot r = new Robot();
+ r.setSpeed(1000);
+ r.penDown();
+	r.setX(50);
+	r.setY(500);
+	r.setPenColor(89,207,79);
+	r.move(100);
+	r.turn(90);
+	r.move(50);
+	r.turn(90);
+	r.move(100);
+}
+void draw 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
